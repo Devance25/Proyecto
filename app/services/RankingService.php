2f33ee280d67260872b09b6612103275bbc67a6b
Pablo Dominguez <pablodominguez@192.168.1.177>
2025-08-31 23:55:36 -0300

    calcularPuntajeService en ranking

--- app/services/RankingService.php
+++ app/services/RankingService.php
@@ -27,4 +27,40 @@ class RankingService
         }
         return self::$instance;
     }
+
+     public function calcularPuntajesService(int $partida_id): array
+    {   
+
+        $colocacionesJugador1 = $this->rankingRepository->getColocacionesRepository($partida_id, 'jugador1');
+
+        $colocacionesJugador2 = $this->rankingRepository->getColocacionesRepository($partida_id, 'jugador2');
+
+        $porRecintoJugador1 = [];
+        foreach($colocacionesJugador1 as $c)
+        {
+            $recinto = $c['recinto'];
+            $tipoDino = $c['tipo_dino'];
+            $porRecintoJugador1[$recinto][] = $tipoDino;
+        }
+
+        $porRecintoJugador2 = [];
+        foreach($colocacionesJugador2 as $c)
+        {
+            $recinto = $c['recinto'];
+            $tipoDino = $c['tipo_dino'];
+            $porRecintoJugador2[$recinto][] = $tipoDino;
+        }
+
+        
+
+        $puntajeJugador1 = $this->puntaje->calcularPuntaje('jugador1', $porRecintoJugador1);
+
+        $puntajeJugador2 = $this->puntaje->calcularPuntaje('jugador2', $porRecintoJugador2);
+
+        return [
+                'jugador1' => $puntajeJugador1,
+                'jugador2' => $puntajeJugador2
+                ];
+
+    }
 }
